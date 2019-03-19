eb8279d927fcb5d20c662c180264d5f14b39ece2
izbz wh <731215820@qq.com>
2019-03-19 18:20:07 +0800

    checkbox样式修改

--- dist/demo.js
+++ dist/demo.js
@@ -8851,9 +8851,7 @@
 	      var onFocusRes = onFocus && onFocus(isExist);
 	      if (onFocusRes instanceof Promise) {
 	        onFocusRes.then(function () {
-	          if (!isExist) {
-	            _this4._focusDom(_this4.selectKeyDomPos, targetDom);
-	          }
+	          _this4._focusDom(_this4.selectKeyDomPos, targetDom);
 	        });
 	      } else {
 	        this._focusDom(this.selectKeyDomPos, targetDom);
